38b79640641cc523c0344ff47b5bb5c5203cfb87
chenhao392 <chenhao.mymail@gmail.com>
2018-11-29 12:27:34 -0500

    fix the cgo call bug by downgrading to go1.6, finished CCA call

--- mLabelCoder.go
+++ mLabelCoder.go
@@ -1,15 +1,20 @@
 package main
 
+//#include <stdlib.h>
+//import "C"
 import (
 	"bufio"
 	"flag"
 	"fmt"
 	linear "github.com/chenhao392/lineargo"
 	"github.com/gonum/matrix/mat64"
+	"github.com/gonum/stat"
+	"log"
 	"math"
 	"os"
 	"strconv"
 	"strings"
+	//"unsafe"
 )
 
 //for cross validation
@@ -49,23 +54,17 @@ func minIdx(inArray []float64) (idx int) {
 }
 
 func computeF1(X *mat64.Dense, Y *mat64.Dense, beta *mat64.Dense) (F1 float64) {
-	//only row stacking available in mat64, the matlab code use "cbind"
-	//X.T()
-	X = mat64.DenseCopyOf(X.T())
-	_, n := X.Caps()
-	//fmt.Println(n)
-	//p2, n2 := beta.Caps()
-	//fmt.Println(p2, n2)
+	//X = mat64.DenseCopyOf(X.T())
+	n, _ := X.Caps()
 	onesSlice := make([]float64, n)
 	for i := range onesSlice {
 		onesSlice[i] = 1
 	}
-	ones := mat64.NewDense(1, n, onesSlice)
-	X2 := mat64.NewDense(0, 0, nil)
-	X2.Stack(ones, X)
-	//p3, n3 := X2.Caps()
-	//fmt.Println(p3, n3)
-	X2 = mat64.DenseCopyOf(X2.T())
+	//ones := mat64.NewDense(1, n, onesSlice)
+	//X2 := mat64.NewDense(0, 0, nil)
+	//X2.Stack(ones, X)
+	//X2 = mat64.DenseCopyOf(X2.T())
+	X2 := colStack(X, onesSlice)
 	Yh := mat64.NewDense(0, 0, nil)
 	Yh.Mul(X2, beta)
 	var tp int
@@ -109,20 +108,20 @@ func computeF1(X *mat64.Dense, Y *mat64.Dense, beta *mat64.Dense) (F1 float64) {
 
 func main() {
 	//input argv
-	//var tsX *string = flag.String("tsX", "data/tsX.txt", "testFeatureSet")
-	//var tsY *string = flag.String("tsY", "data/tsY.txt", "testLabelSet")
+	var tsX *string = flag.String("tsX", "data/tsX.txt", "testFeatureSet")
+	var tsY *string = flag.String("tsY", "data/tsY.txt", "testLabelSet")
 	var trX *string = flag.String("trX", "data/trX.txt", "trainFeatureSet")
 	var trY *string = flag.String("trY", "data/trY.txt", "trainLabelSet")
 	//var inThreads *int = flag.Int("p", 1, "number of threads")
 	flag.Parse()
 	//read data
-	//tsXdata, _, _ := readFile(*tsX, false)
-	//tsYdata, _, _ := readFile(*tsY, false)
+	tsXdata, _, _ := readFile(*tsX, false)
+	tsYdata, _, _ := readFile(*tsY, false)
 	trXdata, _, _ := readFile(*trX, false)
 	trYdata, _, _ := readFile(*trY, false)
 	//vars
-	//nTr, _ := trXdata.Caps()
-	//nTs, _ := tsXdata.Caps()
+	nTr, _ := trXdata.Caps()
+	nTs, _ := tsXdata.Caps()
 	_, nFea := trXdata.Caps()
 	_, nLabel := trYdata.Caps()
 	//lamda
@@ -134,23 +133,50 @@ func main() {
 		nComps[i] = i
 	}
 	//tsY_prob
-	//nRowTsY, _ := tsYdata.Caps()
-	//tsY_prob := make([nROwTsY][nLabel]float64)
+	nRowTsY, _ := tsYdata.Caps()
+	tsY_prob := mat64.NewDense(nRowTsY, nLabel, nil)
 	//tsY_prob := [][]float64{}
-	//for i := 0; i < nLabel; i++ {
-	for i := 0; i < 2; i++ {
-		//LR, regulazor, err := adaptiveTrainLGR_Liblin(trXdata, trYdata.ColView(i), 5, nFea)
-		_, reg, err := adaptiveTrainLGR_Liblin(trXdata, trYdata.ColView(i), 5, nFea)
-		fmt.Println("trained", reg, err)
+	//adding bias term for tsXData
+	ones := make([]float64, nTs)
+	for i := range ones {
+		ones[i] = 1
 	}
+	tsXdata = colStack(tsXdata, ones)
+	//for i := 0; i < nLabel; i++ {
+	for i := 0; i < 1; i++ {
+		wMat, _, _ := adaptiveTrainLGR_Liblin(trXdata, trYdata.ColView(i), 5, nFea)
+		element := mat64.NewDense(0, 0, nil)
+		element.Mul(tsXdata, wMat)
+		for j := 0; j < nTs; j++ {
+			value := 1.0 / (1 + math.Exp(-1*element.At(j, 0)))
+			tsY_prob.Set(j, i, value)
+		}
+	}
+	//adding bias term for trXdata
+	ones = make([]float64, nTr)
+	for i := range ones {
+		ones[i] = 1
+	}
+	trXdata = colStack(trXdata, ones)
 
+	// Calculate the canonical correlations.
+	var cca stat.CC
+	//a, b := trXdata.Caps()
+	//c, d := trYdata.Caps()
+	//fmt.Println(a, b, c, d)
+	err := cca.CanonicalCorrelations(trXdata, trYdata, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	B := cca.Right(nil, true)
+	//fmt.Printf("\n\nlabel projection = %.4f", mat64.Formatted(B.View(0, 0, nLabel-1, nLabel-1), mat64.Prefix("         ")))
 }
-func adaptiveTrainLGR_Liblin(X *mat64.Dense, Y *mat64.Vector, nFold int, nFeature int) (LRmodel *linear.Model, regulator float64, errFinal float64) {
+func adaptiveTrainLGR_Liblin(X *mat64.Dense, Y *mat64.Vector, nFold int, nFeature int) (wMat *mat64.Dense, regulator float64, errFinal float64) {
 	//prior := make([]float64, nFeature)
-	//lamda := [5]float64{0.01, 0.1, 0, 1, 10}
-	//err := []float64{0, 0, 0, 0, 0}
-	lamda := []float64{0.0001, 0.001}
-	err := []float64{0, 0}
+	lamda := []float64{0.0001, 0.001, 0.01, 1, 10}
+	err := []float64{0, 0, 0, 0, 0}
+	//lamda := []float64{0.0001, 0.001}
+	//err := []float64{0, 0}
 	//projMtx := mat64.NewDense(nFeature+1, nFeature+1, nil)
 	//for i := 0; i <= nFeature; i++ {
 	//	projMtx.Set(i, i, 1)
@@ -217,19 +243,12 @@ func adaptiveTrainLGR_Liblin(X *mat64.Dense, Y *mat64.Vector, nFold int, nFeatur
 			//doc in the lineargo lib: If you do not want to change penalty for any of the classes, just set classWeights to nil.
 			//So yes for this implementation, as the penalty not mentioned in matlab code
 			//X: features, Y:label vector, bias,solver,cost,sensitiveness,stop,class_pelnalty
-			LRmodel = linear.Train(trainFold[j].X, trainFold[j].Y, 1.0, 0, 1.0/lamda[i], 0.1, 0.0001, nil)
-			//defer C.free(unsafe.Pointer(LRmodel))
-			//nr_feature := LRmodel.Nfeature()
-			//fmt.Println(nr_feature)
-			//LRmodel.bias = -1
-			w := []float64{-1}
-			//w2 := LRmodel.W()
-			w = append(w, LRmodel.W()...)
-			//LRmodel.w[2:LRmodel.nr_feature] = LRmodel.w[1 : LRmodel.nr_feature-1]
-			//LRmodel.w[1] = b
+			LRmodel := linear.Train(trainFold[j].X, trainFold[j].Y, 1.0, 0, 1.0/lamda[i], 0.1, 0.0001, nil)
+			w := LRmodel.W()
+			lastW := []float64{Pop(&w)}
+			w = append(lastW, w...)
 			wMat := mat64.NewDense(len(w), 1, w)
 			e := 1.0 - computeF1(testFold[j].X, testFold[j].Y, wMat)
-			fmt.Println(e, w[1])
 			err[i] = err[i] + e
 		}
 	}
@@ -241,13 +260,17 @@ func adaptiveTrainLGR_Liblin(X *mat64.Dense, Y *mat64.Vector, nFold int, nFeatur
 	for i := 0; i < Y.Len(); i++ {
 		Ymat.Set(i, 0, Y.At(i, 0))
 	}
-	LRmodel = linear.Train(X, Ymat, 1.0, 0, regulator, 0.1, 0.0001, nil)
+	LRmodel := linear.Train(X, Ymat, 1.0, 0, regulator, 0.1, 0.0001, nil)
+	w := LRmodel.W()
+	lastW := []float64{Pop(&w)}
+	w = append(lastW, w...)
+	wMat = mat64.NewDense(len(w), 1, w)
 	//defer C.free(unsafe.Pointer(LRmodel))
 	//nr_feature := LRmodel.Nfeature() + 1
 	//w := []float64{-1}
 	//w = append(w, LRmodel.W()...)
 	errFinal = err[idx]
-	return LRmodel, regulator, errFinal
+	return wMat, regulator, errFinal
 }
 func readFile(inFile string, rowName bool) (dataR *mat64.Dense, rName []string, err error) {
 	//init
@@ -329,4 +352,21 @@ func Shift(pToSlice *[]string) string {
 	sValue := (*pToSlice)[0]
 	*pToSlice = (*pToSlice)[1:len(*pToSlice)]
 	return sValue
+}
+
+func Pop(pToSlice *[]float64) float64 {
+	pValue := (*pToSlice)[len(*pToSlice)-1]
+	*pToSlice = (*pToSlice)[0 : len(*pToSlice)-1]
+	return pValue
+}
+
+//only row stacking available in mat64, the matlab code use "cbind"
+func colStack(X *mat64.Dense, oneSlice []float64) (X2 *mat64.Dense) {
+	X = mat64.DenseCopyOf(X.T())
+	_, n := X.Caps()
+	ones := mat64.NewDense(1, n, oneSlice)
+	X2 = mat64.NewDense(0, 0, nil)
+	X2.Stack(ones, X)
+	X2 = mat64.DenseCopyOf(X2.T())
+	return X2
 }
